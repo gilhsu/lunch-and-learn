6a45d1f2d0b4ca106d586ef234324122cf2cf1d7
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-03 14:51:08 -0500

    savepoint

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import DayPicker from './DayPicker';
 
 export const App = (props) => {
   return (
     <div>
       <h1>Hello Leila! Superbowl!!!!</h1>
       <img src="https://media.giphy.com/media/BlVnrxJgTGsUw/giphy.gif"></img>
+      <DayPicker />
     </div>
   )
 }
