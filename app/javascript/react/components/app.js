b9c0acad8c53070f97d58158aa4fa09f2af487ba
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-05 17:02:07 -0500

    created react-router and routes for homepage and user show

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,33 +1,17 @@
-import React from 'react'
-import DayPicker from './DayPicker';
+import React from "react";
+import { Route, IndexRoute, Router, browserHistory, Link } from "react-router";
+import HomePage from "../containers/homePage";
+import DashboardContainer from "../containers/dashboardContainer";
 
-export const App = (props) => {
-
+const App = props => {
   return (
     <div>
-      <div id="hero">
-        <div className="row">
-          <div className="small-6" id="hero-text-location">
-            <div className="text1 weight3">
-              Coordinating<br/>
-              Lunch and Learns<br/>
-              Made Easy
-            </div>
-            <div className="text3 weight3" id="hero-description">
-              The <span className="weight5">Lunch<span className="text2 weight5" style={{color: '#11AAB7'}}>&</span>Learn</span> scheduler webapp helps<br/>
-              Sales Teams easily invite and keep track<br/>
-              of their upcoming catered lunch presentations.
-            </div>
-            <a href="www.google.com" className="button large radius" style={{marginTop: '30px'}}>Sign Up</a>
-            <div className="text4 weight4" style={{marginTop: '15px'}}>
-              Already have an account?
-            <a href="www.google.com" style={{fontWeight: 500}}> Log in.</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={HomePage} />
+        <Route path="/users/:id" component={DashboardContainer} />
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
