6ba556df04ea83a4058fd906f0a271849ff41b34
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-06 11:16:52 -0500

    fixed redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  def after_sign_in_path_for(resource)
+    binding.pry
+    "/users/#{resource.id}"
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :company_id, :avatar])
