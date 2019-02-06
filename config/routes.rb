3eb54932ba0e91a37e5b70062e491cc3eacead7f
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-06 16:14:30 -0500

    added routes, controller and react tile

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 
   resources :users, only: [:show]
-  resources :events, only: [:show]
+  resources :events, only: [:show, :new]
 end
