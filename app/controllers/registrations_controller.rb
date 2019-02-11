3e9b11cde470ca78e8d6db1e7689e8efa647699b
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-11 16:30:30 -0500

    save point

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     "/users/#{resource.id}"
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :company_id, :avatar, :profile_photo])
+  end
 end
