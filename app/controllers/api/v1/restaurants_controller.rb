e64374dd390897d2130986f0d31ac9792181d78e
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-14 14:16:17 -0500

    fixed delete restaurants when editing form

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::RestaurantsController < ApplicationController
  def search
-  restaurant_parser = RestaurantParser.new
-  food1 = params[:food1]
-  food2 = params[:food2]
-  location = params[:location]
-  event = Event.find(params[:event])
+   event = Event.find(params[:event])
+   event.restaurants.delete_all
+   restaurant_parser = RestaurantParser.new
+   food1 = params[:food1]
+   food2 = params[:food2]
+   location = params[:location]
 
-  restaurant_parser.search(food1, food2, location, event)
-  render json: { data: restaurant_parser.data }
+   restaurant_parser.search(food1, food2, location, event)
+   render json: { data: restaurant_parser.data }
  end
 end
