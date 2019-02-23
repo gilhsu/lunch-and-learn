1dc4c77281bf990fabcf0b2886ddbd549d935dc0
sketchbook21 <gilbert.hsu2@gmail.com>
2019-02-23 15:23:17 -0500

    new password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
