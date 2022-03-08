fd448b33ab4be5128bc3fe1c09d1fe3bd5b22acc
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-08 16:49:23 +0800

    Delete spec directory

--- spec/support/shoulda.rb
+++
@@ -1,6 +0,0 @@
-require 'shoulda-matchers'
-
-RSpec.configure do |config|
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-end
